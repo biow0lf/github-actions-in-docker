14625fbe1cace632e24234221f6bea08e5581ae0
Igor Zubkov <igor.zubkov@gmail.com>
2023-04-24 20:46:51 +0200

    Cleanup

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd /home/docker/actions-runner || exit
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url "https://github.com/${ORGANIZATION}" --token "${REG_TOKEN}"
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token "${REG_TOKEN}"
 }
 
 trap 'cleanup; exit 130' INT
